83616e9ed1afd547c77f8256564382e818596bc9
Nitish kumar <104833175+nitish-xx@users.noreply.github.com>
2023-01-19 22:14:16 +0530

    Create A very big sum .cpp

---
+++ Problem Solving/A very big sum .cpp
@@ -0,0 +1,22 @@
+https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+   { long long int i;
+    int ar[100];
+    
+    cin>>i;
+    for(int z=0;z<i;z++)
+    {
+        cin>>ar[z];
+    }
+    long long int sum=0;
+    for(int x=0;x<i;x++)
+    {
+        sum+=ar[x];
+    }
+    cout<<sum;
+    
+    return 0;
+}
