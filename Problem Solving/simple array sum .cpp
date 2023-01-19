14afbf6681c8a731d6cb10a5ab19e578f7daa9d8
Nitish kumar <104833175+nitish-xx@users.noreply.github.com>
2023-01-19 22:11:58 +0530

    Create simple array sum .cpp

---
+++ Problem Solving/simple array sum .cpp
@@ -0,0 +1,25 @@
+https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   
+    int i,n,sum;
+    cin>>n;
+    int a[n];
+    for(i=0; i<n; i++){
+        cin>>a[i];
+    }
+    sum = 0;
+    for(i=0; i<n; i++){
+        sum = sum + a[i];
+    }
+    cout<<sum;
+    return 0;
+}
